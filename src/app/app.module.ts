353017f27e2b14593f6d2930200d803efb3d36db
Alex <alex.dasilva@castgroup.com.br>
2024-07-13 14:14:29 -0300

    finalizado a parte do front

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,5 @@
-import { forwardRef, NgModule } from '@angular/core';
-import { FormControl, NG_VALUE_ACCESSOR, ReactiveFormsModule } from '@angular/forms';
+import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -7,10 +7,10 @@ import { AppComponent } from './app.component';
 import { DefaultLoginLayoutComponent } from './components/default-login-layout/default-login-layout.component';
 import { LoginComponent } from './pages/login/login.component';
 import { PrimaryInputComponent } from './components/primary-input/primary-input.component';
-import { provideRouter } from '@angular/router';
 import { provideHttpClient, withFetch } from '@angular/common/http';
 import { ToastrModule } from 'ngx-toastr';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { SignupComponent } from './pages/signup/signup.component';
  
 @NgModule({
   declarations: [
@@ -18,6 +18,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     DefaultLoginLayoutComponent,
     LoginComponent,
     PrimaryInputComponent,
+    SignupComponent,
     
   ],
   imports: [
--- src/app/pages/signup/signup.component.ts
+++ src/app/pages/signup/signup.component.ts
@@ -5,41 +5,43 @@ import { DefaultLoginLayoutComponent } from 'src/app/components/default-login-la
 import { LoginService } from 'src/app/services/login.service';
 import { ToastrService } from 'ngx-toastr';
 
-
-interface LoginForm {
+interface SignupForm {
+  name: FormControl,
   email: FormControl,
-  password: FormControl
+  password: FormControl,
+  passwordConfirm: FormControl
 }
+
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  selector: 'app-signup',
+  templateUrl: './signup.component.html',
+  styleUrls: ['./signup.component.scss']
 })
-export class LoginComponent {
-  loginForm!: FormGroup<LoginForm>;
+export class SignupComponent {
+  signupForm!: FormGroup<SignupForm>;
 
   constructor(
     private router: Router,
     private loginService: LoginService,
     private toastr: ToastrService
   ){
-    this.loginForm = new FormGroup({
+    this.signupForm = new FormGroup({
+      name: new FormControl('', [Validators.required, Validators.minLength(3)]),
       email: new FormControl('', [Validators.required, Validators.email]),
       password: new FormControl('', [Validators.required, Validators.minLength(6)]),
+      passwordConfirm: new FormControl('', [Validators.required, Validators.minLength(6)]),
     });
   }
 
-
 submit(){
-  this.loginService.login(this.loginForm.value.email, this.loginForm.value.password).subscribe({
+  this.loginService.login(this.signupForm.value.email, this.signupForm.value.password).subscribe({
     next: () => this.toastr.success("Login feito com sucesso!"),
       error: () => this.toastr.error("Erro inesperado! Tente novamente mais tarde")
   });
 }
 
 navigate(){
- this.router.navigate(["/signup"]);
+ this.router.navigate(["login"]);
 }
 
 }
-
