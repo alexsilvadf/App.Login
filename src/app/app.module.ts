b54908fddfb904a95696086c732fd8d0e7aa0922
Alex <alex.dasilva@castgroup.com.br>
2024-07-13 08:27:38 -0300

    commit inicial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,23 @@
 import { NgModule } from '@angular/core';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DefaultLoginLayoutComponent } from './components/default-login-layout/default-login-layout.component';
+import { LoginComponent } from './pages/login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DefaultLoginLayoutComponent,
+    LoginComponent,
+    
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/default-login-layout/default-login-layout.component.ts
@@ -0,0 +1,12 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-default-login-layout',
+  templateUrl: './default-login-layout.component.html',
+  styleUrls: ['./default-login-layout.component.scss']
+})
+export class DefaultLoginLayoutComponent {
+@Input() title: string = "";
+@Input() primaryBtnText: string = ""
+@Input() secondaryBtnText: string = ""
+}
