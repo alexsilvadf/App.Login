d50ee50216842611a26412568583ea3097e01a9c
Alex <alex.dasilva@castgroup.com.br>
2024-07-13 11:32:38 -0300

    finalizando toast

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,25 +1,40 @@
-import { NgModule } from '@angular/core';
-import { FormControl, ReactiveFormsModule } from '@angular/forms';
+import { forwardRef, NgModule } from '@angular/core';
+import { FormControl, NG_VALUE_ACCESSOR, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DefaultLoginLayoutComponent } from './components/default-login-layout/default-login-layout.component';
 import { LoginComponent } from './pages/login/login.component';
-
+import { PrimaryInputComponent } from './components/primary-input/primary-input.component';
+import { provideRouter } from '@angular/router';
+import { provideHttpClient, withFetch } from '@angular/common/http';
+import { ToastrModule } from 'ngx-toastr';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+ 
 @NgModule({
   declarations: [
     AppComponent,
     DefaultLoginLayoutComponent,
     LoginComponent,
+    PrimaryInputComponent,
     
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    BrowserAnimationsModule, // required animations module
+    ToastrModule.forRoot(), // ToastrModule added
+    
   ],
-  providers: [],
+  providers: [
+    // provideRouter(routes),
+    provideHttpClient(withFetch()),
+ 
+
+
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,6 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 import { DefaultLoginLayoutComponent } from 'src/app/components/default-login-layout/default-login-layout.component';
+import { LoginService } from 'src/app/services/login.service';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-login',
@@ -9,13 +12,44 @@ import { DefaultLoginLayoutComponent } from 'src/app/components/default-login-la
 })
 export class LoginComponent {
   loginForm!: FormGroup;
+  // snackMsgSuccess = snackMsgSuccess();
 
-  constructor(){
+
+  constructor(
+    private router: Router,
+    private loginService: LoginService,
+    private toastr: ToastrService
+  ){
     this.loginForm = new FormGroup({
       email: new FormControl('', [Validators.required, Validators.email]),
       password: new FormControl('', [Validators.required, Validators.minLength(6)]),
     });
-
   }
 
+  ngOnInit(){
+    // this.snackMsgSuccess('Success','Test');
+ }
+
+
+submit(){
+  this.loginService.login(this.loginForm.value.email, this.loginForm.value.password).subscribe({
+    next: () => this.toastr.success("Login feito com sucesso!"),
+      error: () => this.toastr.error("Erro inesperado! Tente novamente mais tarde")
+  });
 }
+
+navigate(){
+ this.router.navigate(["/signup"]);
+}
+
+}
+
+
+// export const snackMsgSuccess = () => {
+//   const toaster = inject(ToastrService);
+//   return (msg: string,title?: string)=>{
+//     toaster.success(msg, title, {
+//       easeTime: 1000
+//     });
+//   }
+// };
